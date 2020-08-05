4dc45140c493f6da8141b717d4aada78cf14719d
doctor-rutvik14 <rutvik14d@gmail.com>
2020-08-05 21:55:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [Horizontal,setHorizontal] = useState(10);
+  const [Vertical,setVertical] = useState(10);
+  const [Blur,setBlur] = useState(10);
+  const [Color,setColor] = useState("#808080");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 style={{textAlign:"center", marginBottom:"100px"}}>Set boxShadow</h1>
+    <div className="Application">
+      <div className="ranges">
+        <label>Horizontal Length</label>
+        <input type="range" min="-200" max="200" value={Horizontal} onChange = {(e) =>{setHorizontal(e.target.value)} }/>
+        <label>Vertical Length</label>
+        <input type="range" min="-200" max="200" value={Vertical} onChange = {(e) =>{setVertical(e.target.value)} }/>
+        <label>Blur</label>
+        <input type="range" min="0" max="100" value={Blur} onChange = {(e) =>{setBlur(e.target.value)} }/>
+        <label>Color</label>
+        <input type="color" value={Color} onChange = {(e) =>{setColor(e.target.value)} }/>
+      </div>
+      <div className="output">
+        <div className="container" style={{boxShadow:`${Horizontal}px ${Vertical}px ${Blur}px ${Color}` }}>
+        <p>boxShadow: {Horizontal}px  {Vertical}px  {Blur}px  {Color}</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
-
 export default App;
